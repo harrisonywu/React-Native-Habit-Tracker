75ea35b12cd2f7f3de4f3e90867692259da992f9
Harrison Wu <harrison@ridecell.com>
2020-05-19 15:16:03 -0700

    Implement TextInput that submits habit

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [newHabit, setNewHabit] = useState('')
+  const [habits, setHabits] = useState([])
+
+  const createHabit = (() => {
+    const shallowHabits = habits.slice(0)
+    shallowHabits.push(newHabit)
+    setHabits(shallowHabits)
+  })
+
+  useEffect(() => {
+    setNewHabit('')
+  }, [habits, setNewHabit])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        onChangeText={text => {
+          console.log('text: ', text)
+          setNewHabit(text)
+
+        }}
+        onSubmitEditing={createHabit}
+        placeholder="Enter New Habit"
+        style={styles.textInput}
+        value={newHabit}
+      />
     </View>
+
   );
 }
 
@@ -16,4 +40,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    borderColor: 'black',
+    borderStyle: 'solid',
+  }
 });
